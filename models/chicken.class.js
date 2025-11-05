f8f92964471e3f6ac0960091469ecbfadc4e9780
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-11-05 16:02:19 +0100

    Chicken Reset

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,9 +1,10 @@
 class Chicken extends PcNpc {
-    x;
+    x = 700;
     y;
     width = 80;
     height = 90;
     hp = 5;
+    hasReset = false;
 
     offset = {
         top: 10,
@@ -22,21 +23,28 @@ class Chicken extends PcNpc {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.framesWalk);
         this.speed = 0.15 + Math.random() * 0.5,
-
-        this.x = 700 + Math.random() * 300;
+        !this.hasReset ? this.x = 700 + Math.random() * 3000 : this.x = this.x;
         this.y = 480 - this.height - 40;
 
         this.animate();
     }
-    
+
 
     animate() {
         const className = this.constructor.name;
         IntervalManager.setInterval(() => {
             this.checkHealth();
             this.playAnimation(this.framesWalk);
+            this.checkPosition();
         }, 10000 / 60, `${className}chicken-animate`);
         this.moveLeft();
+    }
+
+    checkPosition() {
+        if (this.x < 0) {
+            this.x = 5000;
+            this.hasReset = true;
+        }
     }
 
 }
